014ab266d0ae5c06de851a7343be16951267bf73
Monks09 <mayanksharma5104@gmail.com>
2023-05-11 01:41:01 +0530

    changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import './About.css';
 function About(props) {
     return <div className="about section" id="about">
         <h1 className='section-heading'>About</h1>
-        <p>My name is <span id='user-detail-name'>Mayank Sharma</span>, <span id='user-detail-intro'>I am from Kolkata, India. A Passionate Full Stack Web Developer who likes developing things that dwell in the web universe. I have experience in Frontend frameworks like React and Backend technolgies like NodeJS, ExpressJS and MongoDB.</span> Skilled in problem-solving and execution of software tasks from start to finish. I look forward to joining a company where I can contribute to individual and company growth.</p>
+        <p>Hi, I am <span id='user-detail-name'>Mayank Sharma</span>, <span id='user-detail-intro'>from Kolkata, India. A Passionate Full Stack Web Developer who likes developing things that dwell in the web universe. I have experience in Frontend frameworks like React and Backend technolgies like NodeJS, ExpressJS and MongoDB.</span> Skilled in problem-solving and execution of software tasks from start to finish. I look forward to joining a company where I can contribute to individual and company growth.</p>
     </div>
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import "./Home.css";
 import avatar from "../../assets/img/my-avatar.jpg";
+import backgroundImg from "../../assets/img/portfolio-bg.jpg";
 
 function Home(props) {
     return (
         <div className='home-section' id='home'>
-            <div className='overlay'></div>
-            <div className='titleDiv'>
-                <h2>Hi, I am Mayank</h2>
-                <h2>Full Stack Developer</h2>
-                <div className='resume-div' >
-                    <button id='resume-button-2'>
-                        <a id='resume-link-2' href='resume.pdf' target='_blank' rel="noreferrer" download>Download CV</a>
-                    </button>
+            <img id='background-img' src={backgroundImg} alt='background' />
+            <div className='container'>
+                <div className='titleDiv'>
+                    <h2>Hi, I am Mayank</h2>
+                    <h2>Full Stack Developer</h2>
+                    <div className='resume-div' >
+                        <button id='resume-button-2'>
+                            <a id='resume-link-2' href='resume.pdf' target='_blank' rel="noreferrer" download>Download CV</a>
+                        </button>
+                    </div>
                 </div>
-            </div> 
-            <div className='avatarDiv'>
-                <img id='avatar' className='home-img' src={avatar} alt='avatar' />
+                <div className='avatarDiv'>
+                    <img id='avatar' className='home-img' src={avatar} alt='avatar' />
+                </div>
             </div>
         </div>
     );
